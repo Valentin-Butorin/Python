9a89d5e1989b6a0f100472f2b8cd54cace28c06b
Valentin-Butorin <75623175+Valentin-Butorin@users.noreply.github.com>
2020-12-25 20:00:59 +0800

    Update 4.py

--- Lesson 2/4.py
+++ Lesson 2/4.py
@@ -1,7 +1,4 @@
 string = input()
 string = string.split(' ')
 for i, word in enumerate(string, 1):
-    if len(word) > 10:
-        print(i, word[:10])
-    else:
-        print(i, word[:10])
+    print(i, word[:10])
